7411238c1a318679a38711290f0184c7a6d198a7
ruben <ruben.malchow@gmail.com>
2020-11-26 18:55:11 +0100

    java 11

---
+++ test/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd $(dirname $0)
+export home=$(pwd)
+echo "LDAP home directory is ${home}"
+echo "stopping stack ... "
+docker-compose -p shrtnr down
+docker-compose -p shrtnr rm
+echo "launching containers ... "
+docker-compose -p shrtnr up -d 
