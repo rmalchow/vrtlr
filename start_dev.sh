d81ef7b3723b46e17dc4ed06cd8d7a5ddb4675f6
Gitlab CI <ruben.malchow@gmail.com>
2021-02-11 19:56:51 +0100

    with dev script

--- start_dev.sh
+++ start_dev.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+
+#ln -s ./vrtlr-app/config/application.yml . || true
+docker run -ti -v $(pwd):/app --entrypoint /bin/bash harbor.cloudpowered.services/mcon-group-public/centos8-base:latest -c "cd /app; mvn -s ./settings.dev.xml  -T 16 clean package"
+docker run -ti -v $(pwd):/app -p 8080:8080 --entrypoint /bin/bash harbor.cloudpowered.services/mcon-group-public/centos8-base:latest -c "cd /app; bash scripts/dev.sh"
