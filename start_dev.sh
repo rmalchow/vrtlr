7dd1e4c7efbd6d2b59b7d430be9c5249bdf141ad
Gitlab CI <ruben.malchow@gmail.com>
2021-02-13 09:54:25 +0100

    fix start script

--- start_dev.sh
+++ start_dev.sh
@@ -6,3 +6,6 @@ docker rm -f vrtlr-run
 #ln -s ./vrtlr-app/config/application.yml . || true
 docker run --name vrtlr-build -ti -v $(pwd):/app --entrypoint /bin/bash harbor.cloudpowered.services/mcon-group-public/centos8-base:latest -c "cd /app; mvn -s ./settings.dev.xml  -T 16 clean package"
 docker run --name vrtlr-run -d -v $(pwd):/app -p 8080:8080 --entrypoint /bin/bash harbor.cloudpowered.services/mcon-group-public/centos8-base:latest -c "cd /app; bash scripts/dev.sh"
+
+
+docker logs -f vrtlr-run
