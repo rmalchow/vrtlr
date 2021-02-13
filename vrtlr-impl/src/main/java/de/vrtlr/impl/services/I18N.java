8525782245cf628ede6045e2c59cabee53176b24
Gitlab CI <ruben.malchow@gmail.com>
2021-02-13 12:41:36 +0100

    add i18n setup

---
+++ vrtlr-impl/src/main/java/de/vrtlr/impl/services/I18N.java
@@ -0,0 +1,41 @@
+package de.vrtlr.impl.services;
+
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+@Configuration
+public class I18N implements WebMvcConfigurer {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource msgSrc = new ReloadableResourceBundleMessageSource();
+        msgSrc.setBasename("classpath:i18n/messages");
+        msgSrc.setDefaultEncoding("UTF-8");
+        return msgSrc;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver resolver = new CookieLocaleResolver();
+        resolver.setDefaultLocale(new Locale("en","US"));
+        resolver.setCookieName("myI18N_cookie");
+        return resolver;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry reg) {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("locale");
+        reg.addInterceptor(interceptor);
+    }
+
+}
